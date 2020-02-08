bc8f0c3162fc6b5121fb68d34fcc25a966cf586a
paogrammer <60766499+paogrammer@users.noreply.github.com>
2020-02-08 18:52:49 +0800

    Add files via upload

---
+++ components/CardList.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Card from './Card';
+
+
+
+const CardList = ({robots}) => {
+
+
+	return(
+			<div>
+				{
+					robots.map((user, i) => {
+				
+				return (
+				<Card 
+					key={i} 
+					username={robots[i].username} 
+					name={robots[i].name} 
+					email={robots[i].email}
+					/>
+		 );
+		})
+		}	
+			</div>
+		);
+}
+
+
+export default CardList;
---
+++ robots.js
@@ -0,0 +1,106 @@
+export const robots = [
+	{
+		id: 1,
+		name: 'Miguel Castro',
+		username: 'Migs',
+		email: 'migs@gmail.com'
+	},
+
+	{
+		id: 2,
+		name: 'Joseph Benedict Padua',
+		username: 'Jobi',
+		email: 'jobi@gmail.com'
+	},
+
+	{
+		id: 3,
+		name: 'Loris Trinidad',
+		username: 'Loris',
+		email: 'loris@gmail.com'
+	},
+
+	{
+		id: 4,
+		name: 'John Zedrick Simeon',
+		username: 'Zed',
+		email: 'zed@gmail.com'
+	},
+
+	{
+		id: 5,
+		name: 'Jazmine Estorninos',
+		username: 'Jaz',
+		email: 'jaz@gmail.com'
+	},
+
+	{
+		id: 6,
+		name: 'Anri Ichimura',
+		username: 'Anri',
+		email: 'anri@gmail.com'
+	},
+
+	{
+		id: 7,
+		name: 'Dean Avery Remulla',
+		username: 'Ave',
+		email: 'ave@gmail.com'
+	},
+
+	{
+		id: 8,
+		name: 'Licca Anne Baluyot',
+		username: 'Licca',
+		email: 'licca@gmail.com'
+	},
+
+	{
+		id: 9,
+		name: 'Jessica Clarisse Perez',
+		username: 'Jess',
+		email: 'jess@gmail.com'
+	},
+
+	{
+		id: 10,
+		name: 'Alec Valcory Santiaguel',
+		username: 'Alec',
+		email: 'alec@gmail.com'
+	},
+
+	{
+		id: 11,
+		name: 'Aethan Kyle Javier',
+		username: 'Kyle',
+		email: 'kyle@gmail.com'
+	},
+
+	{
+		id: 12,
+		name: 'Reiner Justin Realica',
+		username: 'Justin',
+		email: 'justin@gmail.com'
+	},
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+]
